5f54576567caf2e0917417c76973cf7281822a2c
monis.q <monis.q@media.net>
2020-05-02 09:29:06 +0530

    keeping minmal changes

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -6,204 +6,204 @@ import HTMLElement from './html';
  * Node Class as base class for TextNode and HTMLElement.
  */
 export default abstract class Node {
-    nodeType: NodeType;
-    childNodes = [] as Node[];
-    children = [] as Node[];
-    text: string;
-    rawText: string;
-    parentNode: Node | null = null;
-    parentElement: HTMLElement | null = null;
-    nextSibling: Node | null = null;
-    previousSibling: Node | null = null;
-    nextElementSibling: Node | null = null;
-    previousElementSibling: Node | null = null;
-    tagName = '';
+	nodeType: NodeType;
+	childNodes = [] as Node[];
+	children = [] as Node[];
+	text: string;
+	rawText: string;
+	parentNode: Node | null = null;
+	parentElement: HTMLElement | null = null;
+	nextSibling: Node | null = null;
+	previousSibling: Node | null = null;
+	nextElementSibling: Node | null = null;
+	previousElementSibling: Node | null = null;
+	tagName = '';
 
-    // Node Types
-    // ELEMENT_NODE= 1;
-    // ATTRIBUTE_NODE= 2;
-    // TEXT_NODE= 3;
-    // CDATA_SECTION_NODE= 4;
-    // ENTITY_REFERENCE_NODE= 5;
-    // ENTITY_NODE= 6;
-    // PROCESSING_INSTRUCTION_NODE= 7;
-    // COMMENT_NODE= 8;
-    // DOCUMENT_NODE= 9;
-    // DOCUMENT_TYPE_NODE= 10;
-    // DOCUMENT_FRAGMENT_NODE= 11;
-    // NOTATION_NODE= 12;
+	// Node Types
+	// ELEMENT_NODE= 1;
+	// ATTRIBUTE_NODE= 2;
+	// TEXT_NODE= 3;
+	// CDATA_SECTION_NODE= 4;
+	// ENTITY_REFERENCE_NODE= 5;
+	// ENTITY_NODE= 6;
+	// PROCESSING_INSTRUCTION_NODE= 7;
+	// COMMENT_NODE= 8;
+	// DOCUMENT_NODE= 9;
+	// DOCUMENT_TYPE_NODE= 10;
+	// DOCUMENT_FRAGMENT_NODE= 11;
+	// NOTATION_NODE= 12;
 
-    protected constructor(parentNode?: Node) {
-        this.parentNode = parentNode || null;
-        if (this.parentNode && this.parentNode.nodeType === NodeType.ELEMENT_NODE) {
-            this.parentElement = this.parentNode as HTMLElement;
-        }
-    }
+	protected constructor(parentNode?: Node) {
+		this.parentNode = parentNode || null;
+		if (this.parentNode && this.parentNode.nodeType === NodeType.ELEMENT_NODE) {
+			this.parentElement = this.parentNode as HTMLElement;
+		}
+	}
 
-    abstract toString(): string;
+	abstract toString(): string;
 
-    /**
-     * Get first child node
-     * @return {Node} first child node
-     */
-    public get firstChild() {
-        return this.childNodes[0] || null;
-    }
+	/**
+	 * Get first child node
+	 * @return {Node} first child node
+	 */
+	public get firstChild() {
+		return this.childNodes[0] || null;
+	}
 
-    /**
-     * Get first child element
-     * @return {Node} first child element
-     */
-    public get firstElementChild() {
-        return this.children[0];
-    }
+	/**
+	 * Get first child element
+	 * @return {Node} first child element
+	 */
+	public get firstElementChild() {
+		return this.children[0];
+	}
 
-    /**
-     * Get last child node
-     * @return {Node} last child node
-     */
-    public get lastChild() {
-        return arr_back(this.childNodes) || null;
-    }
+	/**
+	 * Get last child node
+	 * @return {Node} last child node
+	 */
+	public get lastChild() {
+		return arr_back(this.childNodes) || null;
+	}
 
-    /**
-     * Get last child element
-     * @return {Node} last child element
-     */
-    public get lastElementChild() {
-        return arr_back(this.children) || null;
-    }
+	/**
+	 * Get last child element
+	 * @return {Node} last child element
+	 */
+	public get lastElementChild() {
+		return arr_back(this.children) || null;
+	}
 
-    /**
-     * Remove Child element from childNodes array
-     * @param {HTMLElement} child     node to remove
-     */
-    public removeChild(child: Node) {
-        const childIndex = this.childNodes.indexOf(child);
+	/**
+	 * Remove Child element from childNodes array
+	 * @param {HTMLElement} child     node to remove
+	 */
+	public removeChild(child: Node) {
+		const childIndex = this.childNodes.indexOf(child);
 
-        child.parentNode = null;
+		child.parentNode = null;
 
-        const previousSibling = child.previousSibling || null;
-        const nextSibling = child.nextSibling || null;
-        if (previousSibling) {
-            previousSibling.nextSibling = nextSibling;
-        }
-        if (nextSibling) {
-            nextSibling.previousSibling = previousSibling;
-        }
+		const previousSibling = child.previousSibling || null;
+		const nextSibling = child.nextSibling || null;
+		if (previousSibling) {
+			previousSibling.nextSibling = nextSibling;
+		}
+		if (nextSibling) {
+			nextSibling.previousSibling = previousSibling;
+		}
 
-        if (child.nodeType === NodeType.ELEMENT_NODE) {
-            const previousElementSibling = child.previousElementSibling || null;
-            const nextElementSibling = child.nextElementSibling || null;
-            if (previousElementSibling) {
-                previousElementSibling.nextElementSibling = nextElementSibling;
-            }
-            if (nextElementSibling) {
-                nextElementSibling.previousElementSibling = previousElementSibling;
-            }
-            this.children.splice(this.children.indexOf(child), 1);
-        }
+		if (child.nodeType === NodeType.ELEMENT_NODE) {
+			const previousElementSibling = child.previousElementSibling || null;
+			const nextElementSibling = child.nextElementSibling || null;
+			if (previousElementSibling) {
+				previousElementSibling.nextElementSibling = nextElementSibling;
+			}
+			if (nextElementSibling) {
+				nextElementSibling.previousElementSibling = previousElementSibling;
+			}
+			this.children.splice(this.children.indexOf(child), 1);
+		}
 
-        child.previousSibling = child.nextSibling = null;
-        child.previousElementSibling = child.nextElementSibling = null;
+		child.previousSibling = child.nextSibling = null;
+		child.previousElementSibling = child.nextElementSibling = null;
 
-        return this.childNodes.splice(childIndex, 1);
-    }
+		return this.childNodes.splice(childIndex, 1);
+	}
 
-    /**
-     * Append a child node to childNodes
-     * @param  {Node} node node to append
-     * @return {Node}      node appended
-     */
-    public appendChild<T extends Node = Node>(node: T) {
-        if (node.parentNode) {
-            node.parentNode.removeChild(node);
-        }
-        const lastNode = this.lastChild;
-        if (lastNode) {
-            lastNode.nextSibling = node;
-        }
-        node.previousSibling = lastNode;
+	/**
+	 * Append a child node to childNodes
+	 * @param  {Node} node node to append
+	 * @return {Node}      node appended
+	 */
+	public appendChild<T extends Node = Node>(node: T) {
+		if (node.parentNode) {
+			node.parentNode.removeChild(node);
+		}
+		const lastNode = this.lastChild;
+		if (lastNode) {
+			lastNode.nextSibling = node;
+		}
+		node.previousSibling = lastNode;
 
-        if (node.nodeType === NodeType.ELEMENT_NODE) {
-            const lastElement = this.lastElementChild;
-            node.previousElementSibling = lastElement;
-            this.children.push(node);
-            if (lastElement) {
-                lastElement.nextElementSibling = node;
-            }
-            if (lastNode) {
-                lastNode.nextElementSibling = node;
-            }
-        }
+		if (node.nodeType === NodeType.ELEMENT_NODE) {
+			const lastElement = this.lastElementChild;
+			node.previousElementSibling = lastElement;
+			this.children.push(node);
+			if (lastElement) {
+				lastElement.nextElementSibling = node;
+			}
+			if (lastNode) {
+				lastNode.nextElementSibling = node;
+			}
+		}
 
-        this.childNodes.push(node);
+		this.childNodes.push(node);
 
-        node.parentNode = this;
-        if (this.nodeType === NodeType.ELEMENT_NODE) {
-            node.parentElement = node.parentNode as HTMLElement;
-        } else {
-            node.parentElement = this.parentElement;
-        }
-        return node;
-    }
+		node.parentNode = this;
+		if (this.nodeType === NodeType.ELEMENT_NODE) {
+			node.parentElement = node.parentNode as HTMLElement;
+		} else {
+			node.parentElement = this.parentElement;
+		}
+		return node;
+	}
 
-    /**
-     * Exchanges given child with new child
-     * @param {HTMLElement} oldNode     node to exchange
-     * @param {HTMLElement} newNode     new node
-     */
-    public exchangeChild(oldNode: Node, newNode: Node) {
-        let idx = -1;
-        for (let i = 0; i < this.childNodes.length; i++) {
-            if (this.childNodes[i] === oldNode) {
-                idx = i;
-                break;
-            }
-        }
-        this.childNodes[idx] = newNode;
+	/**
+	 * Exchanges given child with new child
+	 * @param {HTMLElement} oldNode     node to exchange
+	 * @param {HTMLElement} newNode     new node
+	 */
+	public exchangeChild(oldNode: Node, newNode: Node) {
+		let idx = -1;
+		for (let i = 0; i < this.childNodes.length; i++) {
+			if (this.childNodes[i] === oldNode) {
+				idx = i;
+				break;
+			}
+		}
+		this.childNodes[idx] = newNode;
 
-        const previousSibling = oldNode.previousSibling || null;
-        const nextSibling = oldNode.nextSibling || null;
+		const previousSibling = oldNode.previousSibling || null;
+		const nextSibling = oldNode.nextSibling || null;
 
-        newNode.previousSibling = previousSibling;
-        newNode.nextSibling = nextSibling;
+		newNode.previousSibling = previousSibling;
+		newNode.nextSibling = nextSibling;
 
-        if (previousSibling) {
-            previousSibling.nextSibling = newNode;
-        }
-        if (nextSibling) {
-            nextSibling.previousSibling = newNode;
-        }
+		if (previousSibling) {
+			previousSibling.nextSibling = newNode;
+		}
+		if (nextSibling) {
+			nextSibling.previousSibling = newNode;
+		}
 
-        const previousElementSibling = oldNode.previousElementSibling || null;
-        const nextElementSibling = oldNode.nextElementSibling || null;
+		const previousElementSibling = oldNode.previousElementSibling || null;
+		const nextElementSibling = oldNode.nextElementSibling || null;
 
-        newNode.previousElementSibling = previousElementSibling;
-        newNode.nextElementSibling = nextElementSibling;
-        if (newNode.nodeType === NodeType.ELEMENT_NODE) {
-            if (previousSibling) {
-                previousSibling.nextElementSibling = newNode;
-            }
-            if (nextSibling) {
-                nextSibling.previousElementSibling = newNode;
-            }
-            if (previousElementSibling) {
-                previousElementSibling.nextElementSibling = newNode;
-            }
-            if (nextSibling) {
-                nextSibling.previousElementSibling = newNode;
-            }
-        }
-    }
+		newNode.previousElementSibling = previousElementSibling;
+		newNode.nextElementSibling = nextElementSibling;
+		if (newNode.nodeType === NodeType.ELEMENT_NODE) {
+			if (previousSibling) {
+				previousSibling.nextElementSibling = newNode;
+			}
+			if (nextSibling) {
+				nextSibling.previousElementSibling = newNode;
+			}
+			if (previousElementSibling) {
+				previousElementSibling.nextElementSibling = newNode;
+			}
+			if (nextSibling) {
+				nextSibling.previousElementSibling = newNode;
+			}
+		}
+	}
 
-    /**
-     * Exchanges given child with new child
-     * @param {HTMLElement} oldNode     node to exchange
-     * @param {HTMLElement} newNode     new node
-     */
-    public replaceChild(oldNode: Node, newNode: Node) {
-        this.exchangeChild(oldNode, newNode);
-        return oldNode;
-    }
+	/**
+	 * Exchanges given child with new child
+	 * @param {HTMLElement} oldNode     node to exchange
+	 * @param {HTMLElement} newNode     new node
+	 */
+	public replaceChild(oldNode: Node, newNode: Node) {
+		this.exchangeChild(oldNode, newNode);
+		return oldNode;
+	}
 }
--- src/nodes/text.ts
+++ src/nodes/text.ts
@@ -6,42 +6,42 @@ import Node from './node';
  * @param {string} value [description]
  */
 export default class TextNode extends Node {
-    constructor(value: string, parentNode?: Node) {
-        super(parentNode);
-        this.rawText = value;
-    }
+	constructor(value: string, parentNode?: Node) {
+		super(parentNode);
+		this.rawText = value;
+	}
 
-    /**
-     * Node Type declaration.
-     * @type {Number}
-     */
-    nodeType = NodeType.TEXT_NODE;
+	/**
+	 * Node Type declaration.
+	 * @type {Number}
+	 */
+	nodeType = NodeType.TEXT_NODE;
 
-    /**
-     * Get unescaped text value of current node and its children.
-     * @return {string} text content
-     */
-    get text() {
-        return this.rawText;
-    }
+	/**
+	 * Get unescaped text value of current node and its children.
+	 * @return {string} text content
+	 */
+	get text() {
+		return this.rawText;
+	}
 
-    /**
-     * Get unescaped text value of current node and its children.
-     * @return {string} text content
-     */
-    get textContent() {
-        return this.text;
-    }
+	/**
+	 * Get unescaped text value of current node and its children.
+	 * @return {string} text content
+	 */
+	get textContent() {
+		return this.text;
+	}
 
-    /**
-     * Detect if the node contains only white space.
-     * @return {bool}
-     */
-    get isWhitespace() {
-        return /^(\s|&nbsp;)*$/.test(this.rawText);
-    }
+	/**
+	 * Detect if the node contains only white space.
+	 * @return {bool}
+	 */
+	get isWhitespace() {
+		return /^(\s|&nbsp;)*$/.test(this.rawText);
+	}
 
-    toString() {
-        return this.text;
-    }
+	toString() {
+		return this.text;
+	}
 }
